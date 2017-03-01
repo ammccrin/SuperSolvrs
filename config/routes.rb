9470e698f5e45839e17d4e0c912a0bd90a3c24b0
Josica <josica.ganesamurthy@gmail.com>
2017-03-01 11:53:05 -0500

    Add homepage and tutorial level for testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   root 'home#index'
+   get '/tutorial' => 'home#tutorial'
+   get '/level1' => 'home#level1'
+   get '/level2' => 'home#level2'
+   get '/level3' => 'home#level3'
+   get '/level4' => 'home#level4'
+   get '/level5' => 'home#level5'
+   get '/win' => 'home#win'
+   get '/tryagain' => 'home#tryagain'
 end
